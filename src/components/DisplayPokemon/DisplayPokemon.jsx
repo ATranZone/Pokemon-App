1228219e6a479434e96359d9e4467d6bc30f942d
ATranZone <at12550@yahoo.com>
2023-10-21 03:47:08 -0700

    removed a leftover file

--- src/components/DisplayPokemon/DisplayPokemon.jsx
+++ src/components/DisplayPokemon/DisplayPokemon.jsx
@@ -5,11 +5,6 @@ export default function DisplayPokemon(props) {
 		const pokemonName = props.name;
 		const newName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
 		return (
-			// <li id="pokemon-list-item">
-			// 	<p>{props.id}</p>
-			// 	<p>{newName}</p>
-			// 	<img src={props.sprite.front_default}></img>
-			// </li>
 			<tr id="pokemon-row">
 				<td className="pokemon-cell">{props.id}</td>
 				<td className="pokemon-cell">{newName}</td>
